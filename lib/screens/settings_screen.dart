c6325c52788fee306e69332bbe14953719e045af
MohamedSadeck <mohamedferrane19@gmail.com>
2025-02-06 12:39:20 +0100

    the save button to export the json file

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:phones_management/providers/phone_provider.dart';
@@ -5,6 +7,7 @@ import 'package:file_picker/file_picker.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'package:phones_management/models/phone.dart';
+import 'package:path_provider/path_provider.dart'; // updated import
 
 class SettingsScreen extends StatelessWidget {
   static const routeName = '/settings';
@@ -40,6 +43,41 @@ class SettingsScreen extends StatelessWidget {
     }
   }
 
+  Future<void> _exportJsonFile(BuildContext context) async {
+    try {
+      log('Exporting JSON file');
+      final phoneProvider = Provider.of<PhoneProvider>(context, listen: false);
+      await phoneProvider.loadPhones(); // Ensure phones are loaded
+      final jsonList =
+          phoneProvider.phones.map((phone) => phone.toJson()).toList();
+      final jsonContent = json.encode(jsonList);
+
+      // Get the base external storage directory
+      final Directory? baseDir = await getExternalStorageDirectory();
+      if (baseDir != null) {
+        final String downloadsPath =
+            '${baseDir.path.split('Android')[0]}Download';
+        final filePath = '$downloadsPath/phones_export.json';
+        final file = File(filePath);
+        await file.writeAsString(jsonContent);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Phones exported to: $filePath')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Could not access external storage')),
+        );
+      }
+    } catch (e) {
+      log('Error exporting file: $e');
+      print('Error exporting file: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error exporting file: ${e.toString()}')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final phoneProvider = Provider.of<PhoneProvider>(context);
@@ -51,6 +89,11 @@ class SettingsScreen extends StatelessWidget {
             title: const Text('Import JSON from device'),
             leading: const Icon(Icons.file_upload),
             onTap: () => _importJsonFile(context),
+          ),
+          ListTile(
+            title: const Text('Export JSON to device'),
+            leading: const Icon(Icons.file_download),
+            onTap: () => _exportJsonFile(context),
           ),
         ],
       ),
